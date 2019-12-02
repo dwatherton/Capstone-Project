3afc70be3a7e1d48a9a93f4d8ae7396f8a606d35
MinorJinx <jxreynolds1@ualr.edu>
2019-12-01 23:54:39 -0600

    Added Welcome Message on First Load

--- easy_web/static/easy_web/scripts.js
+++ easy_web/static/easy_web/scripts.js
@@ -246,7 +246,64 @@ jQuery(function($) {
 
 /* TODO: Create Scripts For index.html (If We Need Any, Put Them Here!) */
 /***** Start Scripts For index.html *****/
+// jQuery Function For The Popup Welcome Message; Will Occur On First Load Only.
+jQuery(function ($) {
 
+	'use strict';
+
+	$.fn.firstVisitPopup = function (settings) {
+
+		var $body = $('body');
+		var $dialog = $(this);
+		var $blackout;
+		var setCookie = function (name, value) {
+			var date = new Date(),
+				expires = 'expires=';
+			date.setTime(date.getTime() + 2592000000); // Expires in 30 days
+			expires += date.toGMTString();
+			document.cookie = name + '=' + value + '; ' + expires + '; path=/';
+		}
+		var getCookie = function (name) {
+			var allCookies = document.cookie.split(';'),
+				cookieCounter = 0,
+				currentCookie = '';
+			for (cookieCounter = 0; cookieCounter < allCookies.length; cookieCounter++) {
+				currentCookie = allCookies[cookieCounter];
+				while (currentCookie.charAt(0) === ' ') {
+					currentCookie = currentCookie.substring(1, currentCookie.length);
+				}
+				if (currentCookie.indexOf(name + '=') === 0) {
+					return currentCookie.substring(name.length + 1, currentCookie.length);
+				}
+			}
+			return false;
+		}
+		var showMessage = function () {
+			$blackout.show();
+			$dialog.show();
+		}
+		var hideMessage = function () {
+			$blackout.hide();
+			$dialog.hide();
+			setCookie('fvpp' + settings.cookieName, 'true');
+		}
+
+		$body.append('<div id="fvpp-blackout"></div>');
+		$dialog.append('<a id="fvpp-close">&#10006;</a>');
+		$blackout = $('#fvpp-blackout');
+
+		if (getCookie('fvpp' + settings.cookieName)) {
+			hideMessage();
+		} else {
+			showMessage();
+		}
+
+		$(settings.showAgainSelector).on('click', showMessage);
+		$body.on('click', '#fvpp-blackout, #fvpp-close', hideMessage);
+
+	};
+
+});
 /***** End Scripts For index.html *****/
 
 
